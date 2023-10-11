a3bf0f12256a39ff9e2a97a0c6080f9843b5f24e
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-10-11 15:54:47 -0500

    Pricing Util and Tests

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,13 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil(){}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant){
+    float tv = (prevPrice * (0.9 + interest)) * oleoConstant;
+    this->val = tv;
+    return tv;
+}
+
+float PricingUtil::getVal(){
+    return this->val;
+}

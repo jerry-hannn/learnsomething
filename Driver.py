b4c15377e7d842e0ffa2d51884eb8a7ecea5d16f
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-10-11 17:53:08 -0500

    python test

--- Driver.py
+++ Driver.py
@@ -4,3 +4,8 @@ import PricingUtil
 
 if __name__ == "__main__":
     print("Nice work on creating the util! Make sure to check the unit tests!")
+    prevPrice = 2
+    rate = 0.1
+    constant = 10
+    util = PricingUtil.PricingUtil()
+    print(util.get_price(prevPrice, rate, constant))

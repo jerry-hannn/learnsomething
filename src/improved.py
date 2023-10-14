f1e0c9c0d4e28b44eb2caceadd5eb70e65d264e2
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-10-14 15:43:41 -0500

    Remove commented code

--- src/improved.py
+++ src/improved.py
@@ -11,13 +11,3 @@ def max_area(height):
         else:
             left += 1
     return max_area
-
-    # for i in range(n):
-    #     for j in range(i + 1, n):
-    #         h1 = height[i]
-    #         h2 = height[j]
-    #         width = j - i
-    #         current_area = min(h1, h2) * width
-    #         max_area = max(max_area, current_area)
-
-    # return max_area

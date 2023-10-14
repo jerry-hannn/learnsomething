9a36c73dd0867fc9c8cff1e6f93dab5ff4d72f49
jerry-hannn <han.gerald.2@gmail.com>
2023-10-14 14:54:08 -0500

    two pointer sol

--- src/improved.py
+++ src/improved.py
@@ -1,13 +1,23 @@
 def max_area(height):
     max_area = 0
-    n = len(height)
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            h1 = height[i]
-            h2 = height[j]
-            width = j - i
-            current_area = min(h1, h2) * width
-            max_area = max(max_area, current_area)
-
+    left = 0
+    right = len(height) - 1
+    while left < right:
+        max_area = max(max_area, min(height[left], height[right]) *( right - left))
+        if height[left] < height[right]:
+            left += 1
+        elif height[left] > height[right]:
+            right -= 1
+        else:
+            left += 1
     return max_area
+
+    # for i in range(n):
+    #     for j in range(i + 1, n):
+    #         h1 = height[i]
+    #         h2 = height[j]
+    #         width = j - i
+    #         current_area = min(h1, h2) * width
+    #         max_area = max(max_area, current_area)
+
+    # return max_area

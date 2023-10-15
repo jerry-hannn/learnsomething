50274406c15e71322f80f31875973730dcde62ac
jerry-hannn <han.gerald.2@gmail.com>
2023-10-15 02:46:18 -0500

    uhhh implementation?

--- src/interface.py
+++ src/interface.py
@@ -3,11 +3,29 @@ class Exchange:
 
     def __init__(self, initialBalance):
         """Initial Balance is the amount that each account should start with."""
-        pass
+        self.balance = initialBalance
+        self.trades = []
+        self.portfolio = 0
+
 
     def add_trade(self, trade):
         """Adds a trade to the exchange (validation required)
         and returns a match if required. It is up to you on how you will
         handle representing trades. """
-        raise NotImplementedError
+        self.trades.append(trade)
+        trade_cost = trade.quantity * trade.price
+
+        if trade.buy:
+            if trade_cost > self.balance:
+                print("sell your soul first")
+            else:
+                self.balance -= trade_cost
+                self.portfolio += trade.quantity
+        else:
+            if self.portfolio < trade.quantity:
+                print("retail therapy")
+            else:
+                self.balance += trade_cost
+                self.portfolio -= trade.quantity
+
 
